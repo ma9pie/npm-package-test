a0187ebdf524c3b13c057f7e2e6bcbc9f2d764c3
ma9pie <ma9pie.dev@gmail.com>
2024-02-23 20:39:02 +0900

    chore : package name 수정

--- src/pages/@ma9pie/test.tsx
+++ src/pages/@ma9pie/test.tsx
@@ -1,9 +1,11 @@
-import { add, div, mul, sub } from '@ma9pie/test';
+import { add, div, mul, sub, useHelloWorld } from '@ma9pie/test';
 import React, { useEffect, useState } from 'react';
 
 import Layout from '@/components/layouts/Layout';
 
 const Test = () => {
+  useHelloWorld();
+
   useEffect(() => {
     console.log(add(1, 2));
     console.log(sub(1, 2));
