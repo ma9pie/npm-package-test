cdacb873ad16428601bcd354a1176c838099af2f
ma9pie <ma9pie.dev@gmail.com>
2024-03-19 19:11:56 +0900

    chore : package update

--- src/pages/@ma9pie/lite-ui.tsx
+++ src/pages/@ma9pie/lite-ui.tsx
@@ -1,14 +1,24 @@
 import { Flex } from '@ma9pie/lite-ui';
 import React, { useEffect, useState } from 'react';
+import tw, { styled } from 'twin.macro';
 
 import Layout from '@/components/layouts/Layout';
 
 const Test = () => {
   return (
     <Layout>
-      <Flex></Flex>
+      <Flex>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+      </Flex>
     </Layout>
   );
 };
 
 export default Test;
+
+const Box = styled.div`
+  ${tw`w-4 h-4 bg-neutral-500`};
+`;
