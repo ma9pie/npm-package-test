b1e7a44a3a37da506c18ba5809a81858ca9e12c2
ma9pie <ma9pie.dev@gmail.com>
2024-03-22 19:40:57 +0900

    chore : package update

--- src/pages/@ma9pie/lite-ui.tsx
+++ src/pages/@ma9pie/lite-ui.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@ma9pie/lite-ui';
+import { Divider, Flex, Grid, Spacer } from '@ma9pie/lite-ui';
 import React, { useEffect, useState } from 'react';
 import tw, { styled } from 'twin.macro';
 
@@ -7,18 +7,46 @@ import Layout from '@/components/layouts/Layout';
 const Test = () => {
   return (
     <Layout>
-      <Flex col gap={8}>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-      </Flex>
+      <Container>
+        <Flex col gap={8}>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+        </Flex>
+
+        <GridWrapper>
+          <Grid row={2} col={2} gap={8}>
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+          </Grid>
+        </GridWrapper>
+
+        <Flex col gap={8}>
+          <Divider></Divider>
+          <Divider dashed></Divider>
+        </Flex>
+
+        <Flex col>
+          <Box></Box>
+          <Spacer y={8}></Spacer>
+          <Box></Box>
+        </Flex>
+      </Container>
     </Layout>
   );
 };
 
 export default Test;
 
+const Container = styled.div`
+  ${tw`flex flex-col gap-6`};
+`;
 const Box = styled.div`
   ${tw`w-4 h-4 bg-neutral-500`};
+`;
+const GridWrapper = styled.div`
+  ${tw`w-fit`};
 `;
